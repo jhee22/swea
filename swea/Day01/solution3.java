d4783478f3a2ab0ed6c2b49b32c0baf15e5a0b61
현지희 <gucha18@naver.com>
2026-09-01 23:01:22 +0900

    solution3.java

--- swea/Day01/solution3.java
+++ swea/Day01/solution3.java
@@ -13,9 +13,9 @@ public class solution3 {
 
             // input 
             // test -> 5 X 5 
-            int[][] arr = new int[5][5]; 
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
+            int[][] arr = new int[100][100]; 
+            for (int i = 0; i < 100; i++) {
+                for (int j = 0; j < 100; j++) {
                     arr[i][j] = sc.nextInt(); 
                 }
             }
@@ -23,7 +23,7 @@ public class solution3 {
             // logic 
             int answer = 0; 
             // start 
-            int last = arr[0].length - 1; 
+            int last = arr[0].length; 
             for (int j = 0; j < arr[0].length; j++) {
                 if (arr[0][j] == 1) {
                     // path 
@@ -41,24 +41,37 @@ public class solution3 {
                     // (1) 이동했을 때 배열의 범위 안 
                     // (2) Previous State와 동일하면 안됨 
                     // (3) 1 이어야함 
-                    if (curr_j + 1 < last &&  curr_j != prev_j && arr[curr_i][curr_j+1] >= 1) {
-                        // Renew the State  
-                        prev_i = curr_i; 
-                        prev_j = curr_j; 
-                        curr_j = curr_j + 1; 
-                        
-                        if (arr[curr_i][curr_j] == 2) {
-                            answer = path; 
-                        }
+                    while (curr_i < arr.length - 1) {
+                        if (curr_j + 1 < last && !(prev_i == curr_i && prev_j == curr_j + 1) && arr[curr_i][curr_j + 1] >= 1) {
+                            // Renew the State  
+                            prev_i = curr_i; 
+                            prev_j = curr_j; 
+                            curr_j = curr_j + 1; 
                     
+                        } else if (curr_j - 1 >= 0 && !(prev_i == curr_i && prev_j == curr_j - 1) && arr[curr_i][curr_j - 1] >= 1) {
+                            // Renew the State 
+                            prev_i = curr_i; 
+                            prev_j = curr_j; 
+                            curr_j = curr_j - 1; 
 
+                        } else if (curr_i + 1 < last && !(prev_i == curr_i + 1 && prev_j == curr_j) && arr[curr_i + 1][curr_j] >= 1) {
+                            // Renew the State 
+                            prev_i = curr_i; 
+                            prev_j = curr_j; 
+                            curr_i = curr_i + 1; 
+                        } else {
+                            break; 
+                        }
                     }
+                    if (arr[curr_i][curr_j] == 2) {
+                            answer = path; 
+                            break; 
+                    }
+                    
                 }
             }
 
-
-
-            // out 
+            // output
             System.out.println("#" + test_case + " " + answer);
 		}
 	}
