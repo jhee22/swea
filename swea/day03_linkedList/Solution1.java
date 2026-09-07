d07bd02ef57078bc99e1b1502afa8411994c1e89
현지희 <gucha18@naver.com>
2026-09-07 16:00:01 +0900

    day3-2

--- swea/day03_linkedList/Solution1.java
+++ swea/day03_linkedList/Solution1.java
@@ -1,49 +1,42 @@
 package swea.day03_linkedList;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Solution1 {
-       public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-        for(int test_case = 1; test_case <= 10; test_case++) {
+    public static void main(String args[]) throws Exception{
+        // BufferedReader 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         
+        for(int test_case = 1; test_case <= 2; test_case++) {
             // input 
-            int tc  = Integer.parseInt(br.readLine()); 
-            StringTokenizer st = new StringTokenizer(br.readLine()); 
-            Queue<Integer> queue = new ArrayDeque<>(); 
-             
-            for (int i = 0; i < 8; i++) {
-                queue.add(Integer.parseInt(st.nextToken()));
+            LinkedList <Integer> list = new LinkedList<>(); 
+            // StringTokenizer : 특정 한 줄에 붙어 있는 토큰 꺼내기 도구   
+            // 새로운 br.readLine()에서 여러 값을 꺼내야 할 때 새로 만든다.
+            int n = Integer.parseInt(br.readLine()); 
+            StringTokenizer st = new StringTokenizer(br.readLine());     
+            for (int i = 0; i < n; i++) {
+                list.add(Integer.parseInt(st.nextToken())); 
             }
-             
-            // logics 
-            int cnt = 1; 
-            while (!queue.isEmpty() ){
-                int first = queue.poll(); 
-                int value = first - cnt; 
-                 
-                //break
-                if (value <= 0) {
-                    queue.offer(0); 
-                    break; 
-                } else {
-                    queue.offer(value); 
-                }
- 
-                cnt++; 
-                if (cnt > 5) cnt = 1; 
-                
+
+            int m = Integer.parseInt(br.readLine()); 
+            StringTokenizer st2 = new StringTokenizer(br.readLine()); 
+            
+            for (int i = 0; i < m; i++) {
+                String command = st2.nextToken(); // I 
+                int x = Integer.parseInt(st2.nextToken()); 
+                int y = Integer.parseInt(st2.nextToken()); 
+
+                for (int j = 0; j < y; j++) {
+                    int value = Integer.parseInt(st2.nextToken());
+                    list.add(x+j, value); 
+                }   
             }
-             
-            // output
-            System.out.print("#" +  tc + " ");
-            for (int elem : queue) {
-                System.out.printf("%d ", elem);
+            
+            // output 
+            System.out.print("#" + test_case + " ");
+            for (int i = 0; i < 10; i++) {
+                System.out.print(list.get(i) + " "); 
             }
             System.out.println();
-        }
+	    }
     }
 }
