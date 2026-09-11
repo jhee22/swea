dc710bd699bef17958ac07d551c1c7223643e573
현지희 <gucha18@naver.com>
2026-09-11 13:36:50 +0900

    day6-1

---
+++ swea/day06_subset/10726/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*; 
+import java.util.*; 
+public class Solution {
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        int T = Integer.parseInt(br.readLine()); 
+        for (int test_case = 1; test_case <= T; test_case++) {
+            // n, m
+            StringTokenizer st = new StringTokenizer(br.readLine()); 
+            int n = Integer.parseInt(st.nextToken()); 
+            int m = Integer.parseInt(st.nextToken()); 
+
+            
+
+        }
+    }
+}
